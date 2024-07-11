f618307f4101ac831bc3f0b0903829e9ce3261a3
Ishan <ishantiw.quasar@gmail.com>
2024-07-11 12:16:30 +0200

    ♻️ Update deployment scripts

--- scripts/runSepolia.sh
+++ scripts/runSepolia.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-pm2 start pm2.config.js --only testnet-across-relayer-simulated
+cd /home/ubuntu/lisk-across-relayer
+app_name="testnet-across-relayer-simulated"
+pm2 stop $app_name
+pm2 start pm2.config.js --only $app_name
