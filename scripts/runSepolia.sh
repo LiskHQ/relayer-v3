09d5737aa75a2bda1505a95ec07499b0bacff403
Ishan <ishantiw.quasar@gmail.com>
2024-07-11 12:47:42 +0200

    ♻️ Update pm2 settings

--- scripts/runSepolia.sh
+++ scripts/runSepolia.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 app_dir=/home/ubuntu/lisk-across-relayer
 echo "Current DIR: $PWD"
-app_name="testnet-across-relayer-simulated"
-echo "PM2 app name: $app_name"
-pm2 stop $app_name
-echo "Stopped previous app: $app_name"
-pm2 start $app_dir/pm2.config.js --only $app_name
-echo "Started app: $app_name"
+pm2 stop all
+pm2 start pm2.config.js --only "testnet-across-relayer-simulated"
