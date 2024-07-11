bd26d0e6d9113709f9570188796a2d460aa3053f
Ishan <ishantiw.quasar@gmail.com>
2024-07-11 12:32:55 +0200

    ♻️ Update runSepolia script

--- scripts/runSepolia.sh
+++ scripts/runSepolia.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /home/ubuntu/lisk-across-relayer
+app_dir=/home/ubuntu/lisk-across-relayer
 echo "Current DIR: $PWD"
 app_name="testnet-across-relayer-simulated"
 echo "PM2 app name: $app_name"
 pm2 stop $app_name
 echo "Stopped previous app: $app_name"
-pm2 start pm2.config.js --only $app_name
+pm2 start $app_dir/pm2.config.js --only $app_name
 echo "Started app: $app_name"
