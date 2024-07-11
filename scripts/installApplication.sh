dc39c55cd71ffcf54ae61d175e6088b03dfa010c
Ishan <ishantiw.quasar@gmail.com>
2024-07-11 12:16:30 +0200

    ♻️ Update deployment scripts

--- scripts/installApplication.sh
+++ scripts/installApplication.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
 # Install application
+app_dir_name="/home/ubuntu/lisk-across-relayer"
+mkdir app_dir_name
+cd app_dir_name
 echo "Current DIR: $PWD"
 sudo yarn install
 sudo yarn build
