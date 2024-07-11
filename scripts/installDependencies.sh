f30187c2031f795619f58c5c4ff2971d5b9fd16f
Ishan <ishantiw.quasar@gmail.com>
2024-07-11 11:31:49 +0200

    ♻️ Update installDependencies script

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,11 +1,17 @@
 # Install nodejs dependencies
 sudo apt update
+echo "$PWD"
 sudo apt install nodejs
+echo "***Installed nodejs***"
 sudo apt install npm
+echo "***Installed npm***"
+# Install yarn
+sudo npm install --global yarn
+echo "***Installed yarn***"
 # Install pm2
 sudo npm install pm2@latest -g
-# Install yarn
-npm install --global yarn
+echo "***Installed pm2***"
 # Install application
-yarn install
-yarn build
+echo "$PWD"
+sudo yarn install
+sudo yarn build
