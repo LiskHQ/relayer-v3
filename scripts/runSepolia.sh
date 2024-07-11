efff3f4e0709e618a4bedb904c3aaa15e0cbb0ab
Ishan <ishantiw.quasar@gmail.com>
2024-07-11 12:49:24 +0200

    ⚙️ Fix runSepolia script

--- scripts/runSepolia.sh
+++ scripts/runSepolia.sh
@@ -2,4 +2,4 @@
 app_dir=/home/ubuntu/lisk-across-relayer
 echo "Current DIR: $PWD"
 pm2 stop all
-pm2 start pm2.config.js --only "testnet-across-relayer-simulated"
+pm2 start $app_dir/pm2.config.js --only "testnet-across-relayer-simulated"
