907e8d10a8c5fec93ffdcf2f1afb3873f3c231b0
!shan <ishantiw.quasar@gmail.com>
2024-08-12 12:04:03 +0200

    Install required node version (#33)
    ### Description
    
    Add nodejs installation steps to check for required version of node

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,8 +1,47 @@
 # Install nodejs dependencies
 sudo apt update
 echo "Current DIR: $PWD"
-sudo apt install nodejs
-echo "***Installed nodejs***"
+# Script to install Node.js using NVM on Ubuntu without sudo
+
+node_version=v20
+# Function to install NVM
+install_nvm() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+}
+
+# nvm command is a shell function declared in ~/.nvm/nvm.sh
+nvm_path=~/.nvm/nvm.sh
+
+install_node_version() {
+    # Check if NVM is installed
+    if [ -s "$nvm_path" ];
+    then
+        echo "NVM is already installed."
+    else
+        echo "Installing NVM..."
+        install_nvm
+        source nvm_path
+    fi
+
+    node_command=`command -v node`
+    current_node_version=`node -v |  awk -F\. '{print $1}'`
+    # Check if correct version of node is present
+    if [[ "$node_command" ]] && [[ "$node_version" == "$current_node_version" ]];
+    then
+        echo "Correct node version is already installed."
+    else
+        echo "Node version $node_version is not installed. Installing..."
+        nvm install $node_version
+        nvm use $node_version
+    fi
+}
+
+install_node_version
+
+echo "Node.js version $node_version has been installed."
+
 sudo apt install npm
 echo "***Installed npm***"
 # Install yarn
