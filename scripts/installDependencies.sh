df8164046bf7329ed79c4535c0f7dbd671254040
Matjaz Verbole <verbole@gmail.com>
2024-08-14 11:17:56 +0200

    Clean up installDependencies script

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,5 +1,4 @@
 # Install nodejs dependencies
-apt update
 echo "Current DIR: $PWD"
 # Script to install Node.js using NVM on Ubuntu
 
@@ -9,24 +8,23 @@ node_version=$(cat .nvmrc | tr -d "\n")
 # Function to install NVM
 install_nvm() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 }
-
-# nvm command is a shell function declared in ~/.nvm/nvm.sh
-nvm_path=~/.nvm/nvm.sh
 
 install_node_version() {
     # Check if NVM is installed
-    if [ -s "$nvm_path" ];
+    export NVM_DIR="$HOME/.nvm"
+    
+    if [ -s "$NVM_DIR/nvm.sh" ];
     then
         echo "NVM is already installed."
-        source $nvm_path
     else
         echo "Installing NVM..."
         install_nvm
-        source $nvm_path
     fi
+
+    # Setting up environment for NVM    
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
     
     echo "Installing Node version $node_version..."
     nvm install $node_version
@@ -38,7 +36,5 @@ echo "Node.js version $node_version has been installed."
 
 # Install yarn
 npm install --global yarn
-echo "***Installed yarn***"
 # Install pm2
 npm install --global pm2@latest
-echo "***Installed pm2***"
