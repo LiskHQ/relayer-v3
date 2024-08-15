0d6570eefbcf64590dfb9b3414aa9a388d7e9ed1
Matjaz Verbole <verbole@gmail.com>
2024-08-15 18:06:56 +0200

    Fix NVM path inside scripts (#40)
    ### What was the problem?
    
    This PR resolves LISK-944.
    
    ### How was it solved?
    
    NVM path is manually loaded inside `installApplication.sh` script.

--- scripts/installApplication.sh
+++ scripts/installApplication.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-source $HOME/.bashrc
+# Load NVM
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Install application
+# Change to application directory
 app_dir_name=/home/ubuntu/lisk-across-relayer
 cd $app_dir_name
 echo "Current DIR: $PWD"
 
+# Install application
 nvm use
 yarn install --frozen-lockfile
 yarn build
