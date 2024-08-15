99d836ce861ebb67927d0ad65ab616226e8961da
Matjaz Verbole <verbole@gmail.com>
2024-08-15 15:51:43 +0200

    Fix current working directory and remove unneeded mkdir command

--- scripts/installApplication.sh
+++ scripts/installApplication.sh
@@ -2,7 +2,6 @@
 
 # Install application
 app_dir_name=/home/ubuntu/lisk-across-relayer
-mkdir $app_dir_name
 cd $app_dir_name
 echo "Current DIR: $PWD"
 yarn install --frozen-lockfile
--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-
-# Install nodejs dependencies
-echo "Current DIR: $PWD"
 # Script to install Node.js using NVM on Ubuntu
 
+app_dir_name=/home/ubuntu/lisk-across-relayer
+cd $app_dir_name
+echo "Current DIR: $PWD"
+
 # Read node version from .nvmrc file
-node_version=$(cat $PWD/../.nvmrc | tr -d "\n")
+node_version=$(cat .nvmrc | tr -d "\n")
+if [[ -z "${node_version-}" ]];
+then
+    echo ".nvmrc not found; exiting..."
+    exit 1
+fi
 
 # Function to install NVM
 install_nvm() {
@@ -15,8 +21,7 @@ install_nvm() {
 install_node_version() {
     # Setting up environment for NVM    
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-    
+
     # Check if NVM is installed
     if [ -s "$NVM_DIR/nvm.sh" ];
     then
@@ -24,17 +29,20 @@ install_node_version() {
     else
         echo "Installing NVM..."
         install_nvm
-    fi   
-    
+    fi
+
+    # Load NVM
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
     echo "Installing Node version $node_version..."
     nvm install $node_version
 }
 
 install_node_version
-
 echo "Node.js version $node_version has been installed."
 
 # Install yarn
 npm install --global yarn
+
 # Install pm2
 npm install --global pm2@latest
