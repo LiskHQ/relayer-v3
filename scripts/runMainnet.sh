39141af4e78b6d685c1cacab81b1d59e834f7076
Sameer <sameer.subudhi@lisk.com>
2024-08-19 10:29:42 +0200

    Bump targetOverageBuffer for LSK on Lisk (#42)
    ### What was the problem?
    
    This PR resolves
    [LISK-961](https://onchaincollective.atlassian.net/browse/LISK-961)
    
    ### How was it solved?
    
    - [x] Bump `targetOverageBuffer` for LSK on Lisk to `2.5`
    - [x] Drop `MIN_RELAYER_FEE_PCT` to `0.00005`
    
    ### How was it tested?
    
    `yarn relay --wallet void --address <ADDRESS>`
    
    
    [LISK-961]:
    https://onchaincollective.atlassian.net/browse/LISK-961?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- scripts/runMainnet.sh
+++ scripts/runMainnet.sh
@@ -47,7 +47,7 @@ echo "RPC_PROVIDERS_1135=GELATO,DRPC" >> ${app_dir}/.env
 # Mainnet settings
 echo "RELAYER_ORIGIN_CHAINS=[1,1135]" >> ${app_dir}/.env
 echo "RELAYER_DESTINATION_CHAINS=[1,1135]" >> ${app_dir}/.env
-echo "MIN_RELAYER_FEE_PCT=0.0001" >> ${app_dir}/.env
+echo "MIN_RELAYER_FEE_PCT=0.00005" >> ${app_dir}/.env
 
 # Fee settings
 echo "PRIORITY_FEE_SCALER_1=0.8"  >> ${app_dir}/.env
@@ -59,7 +59,7 @@ echo "REDIS_URL='redis://127.0.0.1:6379'" >> ${app_dir}/.env
 # Supported token settings
 echo RELAYER_TOKENS=\'[\"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2\", \"0x6033F7f88332B8db6ad452B7C6D5bB643990aE3f\", \"0xdAC17F958D2ee523a2206206994597C13D831ec7\"]\'  >> ${app_dir}/.env
 echo MIN_DEPOSIT_CONFIRMATIONS=\'{\"5000\": { \"1\": 5, \"1135\": 10 }, \"2000\": { \"1\": 4, \"1135\": 10 }, \"100\": { \"1\": 3, \"1135\": 10 } }\' >> ${app_dir}/.env
-echo RELAYER_INVENTORY_CONFIG=\'{ \"wrapEtherTarget\": 0.1, \"wrapEtherThreshold\": 0.125, \"wrapEtherTargetPerChain\": { \"1\": 1 }, \"wrapEtherThresholdPerChain\": { \"1\": 2 }, \"tokenConfig\": { \"WETH\": { \"1\": { \"targetPct\": 100, \"thresholdPct\": 100, \"unwrapWethThreshold\": 0.5, \"unwrapWethTarget\": 1 }, \"1135\": { \"targetPct\": 30, \"thresholdPct\": 10, \"unwrapWethThreshold\": 0.025, \"unwrapWethTarget\": 0.1, \"targetOverageBuffer\": 1.5 }}, \"LSK\": { \"1135\": { \"targetPct\": 30, \"thresholdPct\": 10, \"targetOverageBuffer\": 1.75 }}, \"USDT\": { \"1135\": { \"targetPct\": 30, \"thresholdPct\": 10, \"targetOverageBuffer\": 1.5 }}}}\' >> ${app_dir}/.env
+echo RELAYER_INVENTORY_CONFIG=\'{ \"wrapEtherTarget\": 0.1, \"wrapEtherThreshold\": 0.125, \"wrapEtherTargetPerChain\": { \"1\": 1 }, \"wrapEtherThresholdPerChain\": { \"1\": 2 }, \"tokenConfig\": { \"WETH\": { \"1\": { \"targetPct\": 100, \"thresholdPct\": 100, \"unwrapWethThreshold\": 0.5, \"unwrapWethTarget\": 1 }, \"1135\": { \"targetPct\": 30, \"thresholdPct\": 10, \"unwrapWethThreshold\": 0.025, \"unwrapWethTarget\": 0.1, \"targetOverageBuffer\": 1.5 }}, \"LSK\": { \"1135\": { \"targetPct\": 30, \"thresholdPct\": 10, \"targetOverageBuffer\": 2.5 }}, \"USDT\": { \"1135\": { \"targetPct\": 30, \"thresholdPct\": 10, \"targetOverageBuffer\": 1.5 }}}}\' >> ${app_dir}/.env
 
 echo "All env vars are set."
 
