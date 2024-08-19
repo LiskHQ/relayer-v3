c33ec782ba123717330a841b92c7bd3d80490733
shuse2 <shus.toda@gmail.com>
2024-08-19 11:27:09 +0200

    feat: update log format

--- src/utils/LogUtils.ts
+++ src/utils/LogUtils.ts
@@ -1,1 +1,37 @@
+// Modify from: https://github.com/UMAprotocol/protocol/blob/master/packages/logger/src/logger/ConsoleTransport.ts
+// Fix to console log in single line, and add error message to log.
+import winston from "winston";
+import { createNewLogger } from "@uma/logger";
+// This transport enables Winston logging to the console.
+const { format } = winston;
+const { combine, timestamp, colorize, printf } = format;
+
+export function createConsoleTransport(): winston.transports.ConsoleTransportInstance {
+  return new winston.transports.Console({
+    handleExceptions: true,
+    format: combine(
+      // Adds timestamp.
+      colorize(),
+      timestamp(),
+      printf((info) => {
+        const { timestamp, level, error, ...args } = info;
+
+        // This slice changes a timestamp formatting from `2020-03-25T10:50:57.168Z` -> `2020-03-25 10:50:57`
+        const ts = timestamp.slice(0, 19).replace("T", " ");
+        // Winston does not properly log Error objects like console.error() does, so this formatter will search for the Error object
+        // in the "error" property of "info", and add the error stack to the log.
+        // Discussion at https://github.com/winstonjs/winston/issues/1338.
+        if (error) {
+          args.errMsg = error;
+        }
+        const log = `${ts} [${level}]: ${Object.keys(args).length ? JSON.stringify(args) : ""}`;
+
+        return log;
+      })
+    ),
+  });
+}
+
+export const Logger = createNewLogger([createConsoleTransport()], { createConsoleTransport: false });
+
 export type DefaultLogLevels = "debug" | "info" | "warn" | "error";
