6d4085bfd43519f7fed167eb9ddc70db6e0a3970
Matjaz Verbole <verbole@gmail.com>
2024-09-12 13:21:28 +0200

    Fix formatting

--- scripts/validateConfig.ts
+++ scripts/validateConfig.ts
@@ -11,8 +11,8 @@ export async function run(): Promise<void> {
       '["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", "0x6033F7f88332B8db6ad452B7C6D5bB643990aE3f", "0xdAC17F958D2ee523a2206206994597C13D831ec7"]',
     MIN_DEPOSIT_CONFIRMATIONS:
       '{"5000": { "1": 5, "1135": 10 }, "2000": { "1": 4, "1135": 10 }, "100": { "1": 3, "1135": 10 } }',
-    RELAYER_ORIGIN_CHAINS: JSON.stringify([1,1135]),
-    RELAYER_DESTINATION_CHAINS: JSON.stringify([1,1135]),
+    RELAYER_ORIGIN_CHAINS: JSON.stringify([1, 1135]),
+    RELAYER_DESTINATION_CHAINS: JSON.stringify([1, 1135]),
     RELAYER_EXTERNAL_INVENTORY_CONFIG: "config/mainnet/relayerExternalInventory.json",
   };
   new RelayerConfig(env);
