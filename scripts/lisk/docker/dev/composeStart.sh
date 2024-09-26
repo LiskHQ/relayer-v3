0db8a5dd5079094042d5ee75df80d550c852b5d3
Incede <33103370+Incede@users.noreply.github.com>
2024-09-26 15:50:58 +0200

    Dev env changes from PR #72

--- scripts/lisk/docker/dev/composeStart.sh
+++ scripts/lisk/docker/dev/composeStart.sh
@@ -15,4 +15,4 @@ cp $HOME/.aws/credentials $PWD/.aws/
 git apply scripts/lisk/docker/dev/docker-compose-dev.patch
 
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ECR_REGISTRY}
-docker compose up --pull always --detach
+docker compose up --pull always --build --detach
