831c98530edc4f1a2abc04e79e1e90b37c4a6e66
Matjaz Verbole <verbole@gmail.com>
2024-10-04 13:49:14 +0200

    Apply review comments

--- scripts/lisk/docker/dev/setEnvVariables.sh
+++ scripts/lisk/docker/dev/setEnvVariables.sh
@@ -4,9 +4,11 @@ set -eu
 echo "Setting environment variables within the current shell on the host"
 
 # Retreive env vars from S3 bucket and source them
-file_name=across-relayer-dev.env
-aws s3 cp s3://lisk-envs/$file_name .${file_name}
-source .${file_name}
-rm -f .${file_name}
+source_env_file_name=across-relayer-dev.env
+env_file_name=.${source_env_file_name}
+
+aws s3 cp s3://lisk-envs/$source_env_file_name ${env_file_name}
+source ${env_file_name}
+rm -f ${env_file_name}
 
 echo "Finished setting all the environment variables within the current shell on the host"
